b9656695562ac17f201931cbfe32c8febf762acd
Enrique <enrique.reyesmarin@gmail.com>
2020-11-09 09:30:54 -0500

    Agregar archivos de proyecto.

---
+++ Consolapk/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolapk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hola mundo//
+            Console.WriteLine("Hola mundo");
+        }
+    }
+}
